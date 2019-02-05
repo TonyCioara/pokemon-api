6e20a028f25ba0327f98263e3f1f2727eb2eac2f
Tony Cioara <tonyangelo9707@gmail.com>
2019-02-05 13:48:08 -0800

    Split routes into functions

--- server.go
+++ server.go
@@ -9,38 +9,42 @@ import (
 	"github.com/labstack/echo"
 )
 
-type Ability struct {
-	Name string `json:"name"`
-	Url  string `json:"url"`
+// type Ability struct {
+// 	Name string `json:"name"`
+// 	Url  string `json:"url"`
+// }
+
+// type Pokemon struct {
+// 	Abilities []Ability
+// }
+func helloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
 
-type Pokemon struct {
-	Abilities []Ability
+func getPokemon(c echo.Context) error {
+	pokemonName := c.QueryParam("name")
+	res, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(data), &result)
+
+	return c.JSON(http.StatusOK, result)
 }
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
 
-	e.GET("/pokemon", func(c echo.Context) error {
-		pokemonName := c.QueryParam("name")
-		res, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+	e.GET("/", helloWorld)
+	e.GET("/pokemon", getPokemon)
 
-		if err != nil {
-			fmt.Println("error")
-		}
-
-		data, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println("error")
-		}
-
-		var result map[string]interface{}
-		json.Unmarshal([]byte(data), &result)
-
-		return c.JSON(http.StatusOK, result)
-	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
